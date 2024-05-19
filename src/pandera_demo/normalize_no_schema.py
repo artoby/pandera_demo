c226647aea45eedfe23b5dd6c08ffa5285ff6122
Artyom Vorobyov <artyom.vorobyov@gmail.com>
2024-05-19 12:12:45 +0200

    Lines reordered

--- src/pandera_demo/normalize_no_schema.py
+++ src/pandera_demo/normalize_no_schema.py
@@ -11,15 +11,16 @@ def normalize_price_no_schema(df_original: pd.DataFrame) -> pd.DataFrame:
             'volume': 'volume_24h_sym1',
         },
     )
+    df['price'] = df['price'].astype(np.float32)
+    df['volume_24h_sym1'] = df['volume_24h_sym1'].astype(np.float32)
 
     symbol_map_df = pd.DataFrame({'symbol': df_original['symbol'].unique()})
     symbol_map_df[['symbol1', 'symbol2']] = symbol_map_df['symbol'].apply(extract_pair_symbols).to_list()
     map_sym1 = dict(symbol_map_df[['symbol', 'symbol1']].values)
     map_sym2 = dict(symbol_map_df[['symbol', 'symbol2']].values)
-    df['price'] = df['price'].astype(np.float32)
-    df['volume_24h_sym1'] = df['volume_24h_sym1'].astype(np.float32)
     df['symbol1'] = df_original['symbol'].map(map_sym1).astype('category')
     df['symbol2'] = df_original['symbol'].map(map_sym2).astype('category')
+
     df['timestamp'] = pd.to_datetime(df['scrape_timestamp'])
 
     return df
